26f214b250890cbd13f8533d1ee37f202a36bd40
hamzaacode <amir.hamza.code@gmail.com>
2023-10-30 11:01:43 +0600

    Stream Map Example using OOP concept

---
+++ src/main/java/IntermediateOperations/StreamMapExample.java
@@ -0,0 +1,40 @@
+package IntermediateOperations;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Person {
+    private String name;
+    
+    public Person(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class StreamMapExample {
+    public static void main(String[] args) {
+        // Create a list of persons
+        List<Person> persons = List.of(
+            new Person("Alice"),
+            new Person("Bob"),
+            new Person("Charlie"),
+            new Person("David")
+        );
+        
+        // Use Java streams to map names to a new list
+        List<String> names = persons.stream()
+            .map(Person::getName) // Using a method reference
+            .collect(Collectors.toList());
+        
+        // Print the mapped names
+        names.forEach(System.out::println);
+    }
+}
