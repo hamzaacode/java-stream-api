00ce068871192f1d7178ef11468f048151cdb949
hamzaacode <amir.hamza.code@gmail.com>
2023-10-30 10:50:47 +0600

    Stream Creation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Stream<Integer> stream = Main.streamCreation();
+    }
+
+    private static Stream<Integer> streamCreation() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        return numbers.stream();
+    }
 }
